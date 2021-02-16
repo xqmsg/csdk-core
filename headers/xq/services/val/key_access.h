511d0c33cb4ebecc75a1ea6e0fb091a654e21980
Ike E <social@xqmsg.com>
2021-02-16 12:35:37 -0500

    Update key_access.h documentation.

--- headers/xq/services/val/key_access.h
+++ headers/xq/services/val/key_access.h
@@ -27,6 +27,9 @@ _Bool xq_svc_grant_access( struct xq_config* config,
 /// request must be the same one that generated the key.
 ///
 /// @param config The XQ configuration instance.
+/// @param token The encryption key token.
+/// @param recipients A list of user accounts (email address/phone) to revoke access.
+/// @param recipient_count The number of recipients to add.
 /// @param error An optional, user-provided block  to store details of any error that occurs.
 _Bool xq_svc_revoke_access(   struct xq_config* config,
                            char* token,
